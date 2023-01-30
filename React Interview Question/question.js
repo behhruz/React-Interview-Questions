84ea2e10fdf22eb0f9d49a6b382876bbcc007980
behhruz <bsatimbayev05@gmail.com>
2023-01-30 16:24:54 +0500

    react

--- React Interview Question/question.js
+++ React Interview Question/question.js
@@ -1,3 +1,46 @@
 
-// React nima 
-// Bu javascriptni kutubhonasi 
+//  1 React nima? 
+// Bu javascriptni kutubhonasi,bu bizaga bir sahifali pagelar va prilojeniyalar yaratishimizga yordam beradi
+// Djordan Uoke tomonidan yani facebook developerlari tomonidan yaratilgan 2011 Facebookda ciqgan 2012 yil Instagramda
+
+// 2 Reactning assosiy hususiyatlari qanday?
+// Jsx sintaxisini ishlatadi.Yani Javascriptda Html kodlarini yozish.
+// RealDOM manipulyatsiyasi qimmat ekanligini hisobga olib, RealDOM o'rniga VirtualDOM-dan foydalanadi.
+// Server tomonidan Rendering holatini padderjka qiladi.
+// Bir yo'nalishli ma'lumotlar oqimini yoki ma'lumotlarni bog'lashni kuzatib boradi
+
+// 3 Jsx nima?
+// Bu Javascript XML
+// React.createElement() sintaxis sugari desak boladi,chunki bizga 
+// HTML kabi shablon sintaksisi bilan birga JavaScript-ning ifodaliligini beradi.
+
+// NodeModuls nima?
+// React ishlatadigon kutubhonalari
+// reactni qoshimcha dependensieslari joylashadi borib
+
+// Public nima?
+// single page aplication
+// hamma yozvotgan narsalarimiz borib eng katta va bitta index.html ichidagi root ga borib ulanadi
+
+// .gitIgnore nima?
+// githubga ketayotgan skritiy faylani yashirib qoyish.
+
+// package.json 
+// projectni passporti desak boladi 
+// ichidagi malumotlarini ozimiz ozgartirib qoyolmaymiz
+// yani npm bilan tortib kelgan kutubhonalarimiz ham joylashadi
+
+
+// Mouting => yaratishlik class constructor super render bolib keyin ComponentDidMount boladi
+
+// Updating => rerender qilish yani SetState  bolib keyin ComponentDidUpdate boladi
+
+// Unmouting => bizga qachonki komponent ochib ketayotganida yoki ochib
+//  qoganda Domdan shunda ishlatamiz ComponentWillUnmouting
+
+
+// UseEffect => malumotlarini bir marta rerender qilish yani birinchisi objectda malumotla kiritiladi ikkinchi 
+// qiymati bosh massiv yani shu malumnotlarni bir marta render qilib berishga
+
+
+
